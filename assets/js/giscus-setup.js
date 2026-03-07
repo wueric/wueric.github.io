ada140358466bf04711a641db292639bf7bdbfe9
wueric <wueric@users.noreply.github.com>
2026-03-07 18:28:43 +0000

    Deploying to gh-pages from @ wueric/wueric.github.io@7236c3bbeba88ef167cf87119b60fcfdf947032b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "wueric/main-page",
+    "data-repo": "wueric/wueric.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
